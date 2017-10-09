c7c6f60cac602a6f0f77499744c23d39b4522056
Timur <tzaynashev@bk.ru>
2017-10-09 12:53:54 +0600

    тест гита

--- reportFromWaterApp/Form1.cs
+++ reportFromWaterApp/Form1.cs
@@ -639,6 +639,7 @@ namespace reportFromWaterApp
         //автоматическое обновление 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            //если датагридвью == текущей дате то:
             if (dataPicker.Value.Date == DateTime.Now.Date)
             {
                 dataGridViewContainer();
